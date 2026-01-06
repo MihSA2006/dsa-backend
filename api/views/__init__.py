34d2e93ddb0a911a0c0e7de10324051aec75101d
unknown <misandratra.harena3@gmail.com>
2026-01-06 10:00:20 +0300

    Reorganize views contest

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -2,5 +2,3 @@ from .Challenges import ChallengeViewSet , TestCaseViewSet
 from .ChallengeAction import join_challenge, test_challenge_solution,test_specific_test_case, submit_challenge_solution, my_challenges, save_code
 from .Leaderboard import challenge_leaderboard, global_leaderboard, my_stats
 from .Other import ExecuteCodeView, HealthCheckView, SupportedLanguagesView, SecurityInfoView
-
-
