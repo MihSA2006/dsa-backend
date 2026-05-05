f1a46c3860a13ec97020625b8ecabafea37be7b3
Henintsoa <hentsraf@gmail.com>
2026-05-05 11:14:22 +0300

    Essai.

--- backend/settings.py
+++ backend/settings.py
@@ -67,26 +67,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config("DB_NAME"),
+#         'USER': config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         'HOST': config("DB_HOST"),
+#         'PORT': config("DB_PORT"),
+#         'OPTIONS': {
+#             'sslmode': 'require',
+#         },
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
@@ -102,6 +102,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://nextjs-1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://dsa.insi-university.com",
+    
 ]
 
 REST_FRAMEWORK = {
